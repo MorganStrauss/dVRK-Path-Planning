a53c67680b305704c5379bbce181619449d80858
MorganStrauss <31320545+MorganStrauss@users.noreply.github.com>
2022-12-11 18:43:47 -0500

    Add files via upload

--- neighborPotentials.m
+++ neighborPotentials.m
@@ -1,17 +1,30 @@
 function [neighborP] = neighborPotentials(field,row,n)
+    P_curr = field(row,4);
     for i =-1:2:1 %Go forward and backward in x:
          currentRow = row+(n^2*i);
-         P_new = field(currentRow,4);
+         if currentRow < 1 || currentRow > n^3
+            P_new = 1.25*P_curr;
+         else
+            P_new = field(currentRow,4);
+         end
          neighborP(0.5*i+1.5,1) = p_new;
     end
     for i =-1:2:1 %Go forward and backward in y:
         currentRow = row+n*i;
-        P_new = field(currentRow,4);
+        if currentRow < 1 || currentRow > n^3
+            P_new = 1.25*P_curr;
+        else
+            P_new = field(currentRow,4);
+        end
         neighborP(0.5*i+1.5,2) = p_new;
     end
     for i =-1:2:1 %Go forward and backward in z:
         currentRow = row+i;
-        P_new = field(currentRow,4);
+        if currentRow < 1 || currentRow > n^3
+            P_new = 1.25*P_curr;
+        else
+            P_new = field(currentRow,4);
+        end
         neighborP(0.5*i+1.5,3) = p_new;
     end
 end
