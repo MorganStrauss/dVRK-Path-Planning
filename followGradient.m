a03e01fd8e434d18a8f1d9e230089b79c5dd1712
MorganStrauss <31320545+MorganStrauss@users.noreply.github.com>
2022-12-11 15:18:46 -0500

    Add files via upload

--- followGradient.m
+++ followGradient.m
@@ -1,12 +1,16 @@
-function trajectory = followGradient(startTheta,gradient,goalTheta)
+function trajectory = followGradient(startTheta,gradientField,goalTheta)
     %Inputs: p - starting position (js), gradient - gradient that you want to follow
     %Outputs: trajectory - overall trajectory after following gradient
     i =1;
     e = 0.02*(0.15-0028);
     while(norm(startTheta-goalTheta)>e && i < 100) %Set precision based on theta3 bounds
-        trajectory(i,:) = startTheta;
-        row = findClosest(startTheta,gradient);
-        startTheta = startTheta + gradient(row,5:7)';
+        if abs(startTheta(1)) > max(gradientField(:,1)) || abs(startTheta(2)) > max(gradientField(:,2)) || startTheta(3) > max(gradientField(:,3)) || startTheta(3) < min(gradientField(:,3))
+            error('Trajectory lead out of bounds')
+        end
+        trajectory(i,:) = startTheta';
+        row = findClosest(startTheta,gradientField);
+        push = gradientField(row,5:7)';
+        startTheta = startTheta + gradientField(row,5:7)';
         i = i+1;
     end
     save('Trajectory.mat','trajectory')
