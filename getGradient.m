1d6849cee599bf006de4ae1985e234109d6351b3
MorganStrauss <31320545+MorganStrauss@users.noreply.github.com>
2022-12-11 19:58:46 -0500

    Add files via upload

--- getGradient.m
+++ getGradient.m
@@ -1,33 +1,17 @@
 function newField = getGradient(field,n)
     %Inputs: field - 7 column matrix with columns of:
         % theta1, theta2, theta3, Potential, and x,y,z index
-        gradient = zeros(length(field),3);
-    for i = (n+n^2+2):(n-2)*n^2+(n-2)*n+(n-1)
-        gradient(i,:) = nearestNeighbor(field,i,n);
+        dx = field(n^2+1,1) - field(1,1);
+        dy = field(n+1,2) - field(1,2);
+        dz = field(2,3) - field(1,3);
+    for i = 1:length(field(:,1))
+        currentP = field(i,4);
+        neighborP = neighborPotentials(field,i,n);
+        xGradient(i,1) = (neighborP(1,1)-currentP+currentP-neighborP(2,1))/(2*dx);
+        yGradient(i,1) = (neighborP(1,2)-currentP+currentP-neighborP(2,2))/(2*dy);
+        zGradient(i,1) = (neighborP(1,3)-currentP+currentP-neighborP(2,3))/(2*dz);
     end
-    newField = [field(:,1:4) gradient field(:,5:7)];
+    newField = [field(:,1:4) xGradient yGradient zGradient field(:,5:7)];
+    quiver3(newField(:,1),newField(:,2),newField(:,3),newField(:,5),newField(:,6),newField(:,7),'r')
 end
 
-function [gradient] = nearestNeighbor(field,row,n)
-    theta1 = field(row,1);
-    theta2 = field(row,2);
-    theta3 = field(row,3);
-    P = field(row,4);
-    P_curr = P;
-    gradient = [0 0 0]; %Gradient if no surrounding potential is lower
-    for i =-1:1
-      for j=-1:1
-          for h = -1:1
-              currentRow = row+(n^2*i)+n*j+h;
-              theta1_new = field(currentRow,1);
-              theta2_new = field(currentRow,2);
-              theta3_new = field(currentRow,3);
-              P_new = field(currentRow,4);
-              if P_new < P_curr
-                  gradient = (P_new-P)./[theta1_new-theta1,theta2_new-theta2,theta3_new-theta3];
-                  P_curr = P_new;
-              end
-          end
-      end
-    end
-end
--- neighborPotentials.m
+++ neighborPotentials.m
@@ -3,28 +3,28 @@ function [neighborP] = neighborPotentials(field,row,n)
     for i =-1:2:1 %Go forward and backward in x:
          currentRow = row+(n^2*i);
          if currentRow < 1 || currentRow > n^3
-            P_new = 1.25*P_curr;
+            P_new = P_curr;
          else
             P_new = field(currentRow,4);
          end
-         neighborP(0.5*i+1.5,1) = p_new;
+         neighborP(0.5*i+1.5,1) = P_new;
     end
     for i =-1:2:1 %Go forward and backward in y:
         currentRow = row+n*i;
         if currentRow < 1 || currentRow > n^3
-            P_new = 1.25*P_curr;
+            P_new = P_curr;
         else
             P_new = field(currentRow,4);
         end
-        neighborP(0.5*i+1.5,2) = p_new;
+        neighborP(0.5*i+1.5,2) = P_new;
     end
     for i =-1:2:1 %Go forward and backward in z:
         currentRow = row+i;
         if currentRow < 1 || currentRow > n^3
-            P_new = 1.25*P_curr;
+            P_new = P_curr;
         else
             P_new = field(currentRow,4);
         end
-        neighborP(0.5*i+1.5,3) = p_new;
+        neighborP(0.5*i+1.5,3) = P_new;
     end
 end
